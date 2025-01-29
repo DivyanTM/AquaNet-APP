9262bb62ab7bcb41e04c4e61616e77a4cf04acca
DivyanTM <divyanthiruvalarselvan@gmail.com>
2025-01-30 00:56:57 +0530

    added menu bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.divyan.aquanet"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.divyan.aquanet"
--- app/src/main/java/com/divyan/aquanet/MainActivity.java
+++ app/src/main/java/com/divyan/aquanet/MainActivity.java
@@ -1,24 +1,91 @@
 package com.divyan.aquanet;
 
+import static com.divyan.aquanet.R.id.navigation_view;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+
+    private MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.frame_content), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+
+        setSupportActionBar(toolbar);
+
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        toolbar.setNavigationOnClickListener(view -> {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            handleNavigationClick(item);
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void handleNavigationClick(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.inbox_item) {
+            Toast.makeText(this, "Inbox Clicked", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.outbox_item) {
+            Toast.makeText(this, "Outbox Clicked", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.favourites_item) {
+            Toast.makeText(this, "Favourites Clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
